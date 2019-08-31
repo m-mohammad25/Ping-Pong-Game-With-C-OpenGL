a0161f21f14f3a20c9d924477f0bca4db940f36a
Mohammad Mohammad <barcelona100600@gmail.com>
2019-08-31 17:07:50 +0300

    start working with ball hit paddel

--- CG/GLClass.cs
+++ CG/GLClass.cs
@@ -11,11 +11,12 @@ namespace CG
 {
     public class GLClass : OpenGLControl
     {
+        private float Hors_bound = 2.85f;
         private int Score_P1 = 0, Score_P2 = 0;
-        private const float Pad_bound = 2.09f , Pad_height=1.3f,Pad_width=2,Pad_velo=0.5f;
+        private const float Pad_bound = 2.09f, Pad_height=1.3f,Pad_width=2,Pad_velo=0.5f;
         private float P1_Pad_y = 0, P2_Pad_y = 0, Pad_x = 7;
-        private float ball_velo_x = 0, ball_velo_y = 0, speed_inc = 0.5f;
-        private int ball_pos_x = 0, ball_pos_y = 0, ball_rad = 20;
+        private float ball_velo_x = -0.0003f, ball_velo_y = 0.0005f, speed_inc = 0.5f;
+        private float ball_pos_x = 0f, ball_pos_y = 0, ball_rad = 0.15f;
         private bool P1_up_press = false, P1_down_press = false,
                      P2_up_Press = false, P2_down_press = false;
 
@@ -37,6 +38,17 @@ namespace CG
             GL.glVertex2f(Pad_width, -height);
             GL.glVertex2f(-Pad_width, -height);
             GL.glEnd();
+
+            GL.glPopMatrix();
+        }
+
+        void drawBall(float x,float y)
+        {
+            GL.glPushMatrix();
+
+            GL.glTranslatef(x, y, 0);
+            GL.glColor3f(1.0f, 0, 0);
+            GL.glutSolidSphere(ball_rad, 20, 16);
 
             GL.glPopMatrix();
         }
@@ -65,10 +77,34 @@ namespace CG
             drawPaddle(-Pad_x, P1_Pad_y);
 
             drawPaddle(Pad_x, P2_Pad_y);
+
+            drawBall(ball_pos_x,ball_pos_y);
             this.Refresh();
             
         }
-       
+
+        public void play()
+        {
+            ball_pos_x += ball_velo_x;
+            ball_pos_y += ball_velo_y;
+
+            if (ball_pos_y + ball_rad > Hors_bound || ball_pos_y - ball_rad < -Hors_bound)
+            {
+                ball_velo_y = -ball_velo_y;
+            }
+
+            if(ball_pos_x - ball_rad - Pad_width < -Pad_x && ball_pos_x - ball_rad  < -Pad_x)
+            {
+                if (ball_pos_y < P1_Pad_y + Pad_height && ball_pos_y > P1_Pad_y - Pad_height)
+                {
+                    ball_velo_x = -ball_velo_x;
+                    //ball_velo_x += speed_inc;
+                }
+            }
+
+
+        }
+
         public override void Refresh()
         {
             base.Refresh();
@@ -98,10 +134,11 @@ namespace CG
 
             }
 
+            play();
 
         }
 
-        float step = 0;
+       
 
        
         protected override void OnSizeChanged(EventArgs e)
